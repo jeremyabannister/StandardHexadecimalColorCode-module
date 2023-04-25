d321b1a8e4869e70260b0ab549197ad7a7c6ca6e
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-25 19:24:06 +0200

    Initial Commit

---
+++ Tests/StandardHexadecimalColorCode-moduleTests/StandardHexadecimalColorCode_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import StandardHexadecimalColorCode_module
+
+final class StandardHexadecimalColorCode_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(StandardHexadecimalColorCode_module().text, "Hello, World!")
+    }
+}
