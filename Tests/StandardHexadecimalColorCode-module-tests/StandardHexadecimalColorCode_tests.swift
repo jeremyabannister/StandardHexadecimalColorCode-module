5d224537328e08954cf9d0493f9798723e2ba8a3
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-26 04:54:42 +0200

    Added `StandardHexadecimalColorCode` (among other things).

---
+++ Tests/StandardHexadecimalColorCode-module-tests/StandardHexadecimalColorCode_tests.swift
@@ -0,0 +1,61 @@
+//
+//  StandardHexadecimalColorCode_tests.swift
+//  
+//
+//  Created by Jeremy Bannister on 4/25/23.
+//
+
+///
+final class StandardHexadecimalColorCode_tests: XCTestCase {
+    
+    ///
+    func test_init_rgba () throws {
+        
+        ///
+        func randomizedTest () throws {
+            
+            ///
+            let rgba: ColorCode_RGBA_256 = .generateRandom()
+            
+            ///
+            try StandardHexadecimalColorCode(
+                rgba: rgba
+            )
+                .assert(\.rgbaColorCode, equals: rgba)
+        }
+        
+        ///
+        for _ in (0 ... 1_000) {
+            try randomizedTest()
+        }
+    }
+}
+
+///
+extension StandardHexadecimalColorCode_tests {
+    
+    ///
+    func test_init_Optional_String () throws {
+        
+        ///
+        func randomizedTest () throws {
+            
+            ///
+            let rgba = ColorCode_RGBA_256.generateRandom()
+            
+            ///
+            try! StandardHexadecimalColorCode(rgba.asHexString)
+                .assertEqual(to: .init(rgba: rgba))
+        }
+        
+        ///
+        for _ in (0 ... 1_000) {
+            try randomizedTest()
+        }
+    }
+}
+
+
+
+///
+extension StandardHexadecimalColorCode: Testable { }
